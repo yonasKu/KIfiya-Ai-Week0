0ded11f2a0276836478a4abf5dab124fa776a224
yonas <yonijonahphineas0@gmail.com>
2024-08-24 22:08:17 +0300

    first commit

--- scripts/bubble_charts.py
+++ scripts/bubble_charts.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('path_to_your_data.csv')
+data = pd.read_csv('../data/bubble_charts.py')
 
 plt.figure(figsize=(10, 6))
 plt.scatter(data['GHI'], data['Tamb'], s=data['RH'], alpha=0.5)
 plt.xlabel('GHI')
+plt.ylabel('Tamb')
 plt.ylabel('Tamb')
 plt.title('GHI vs Tamb vs RH')
 plt.show()
